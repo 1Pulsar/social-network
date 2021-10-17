3e7fa5c38eeebbd72ff671df899b00a19a61a5b9
1Pulsar <91329739+1Pulsar@users.noreply.github.com>
2021-10-17 20:53:40 +0300

    Combined conteiner and class component

--- src/components/Content/Finder/Finder.jsx
+++ src/components/Content/Finder/Finder.jsx
@@ -1,74 +1,53 @@
 import React from "react"
 import st from "./Finder.module.css";
-import * as axios from "axios";
 
-class Finder extends React.Component {
-    componentDidMount() {
-        axios
-            .get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-                this.props.setTotalCount(response.data.totalCount)
-            })
+const Finder = (props) => {
+
+    const totalPages = Math.ceil (props.totalCount/props.pageSize)
+    let pagesArray = []
+    for (let i = props.currentPage - 2; i<=props.currentPage + 1; i++) {
+        if (i < 1 || i > totalPages) {
+            continue
+        } else {
+            pagesArray.push(i)
+        }
+    }
+    const changeCurrentPage = (p) => {
+        props.changeCurrentPage(p)
     }
 
-    componentWillUnmount() {
-        this.props.changePage(1)
-    }
+    return <div>
 
-    render() {
-        const totalPages = Math.ceil (this.props.totalCount/this.props.pageSize)
-        let pagesArray = []
-        for (let i = this.props.currentPage - 2; i<=this.props.currentPage + 1; i++) {
-            if (i < 1 || i > totalPages) {
-                continue
-            } else {
-                pagesArray.push(i)
-            }
-        }
-        const changeCurrentPage = (p) => {
-            this.props.changePage(p)
-            axios
-                .get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${p}`)
-                .then(response => {
-                    this.props.setUsers(response.data.items)
-                    this.props.setTotalCount(response.data.totalCount)
-                })
-        }
+        <div className={st.pagination}>
 
-        return <div>
+            {pagesArray[0] > 1?<span onClick={ () => changeCurrentPage(1) }>1..</span>:''}
 
-            <div className={st.pagination}>
-
-                {pagesArray[0] > 1?<span onClick={ () => changeCurrentPage(1) }>1..</span>:''}
-
-            {pagesArray.map(p => <span className={p === this.props.currentPage ? st.currentPage:''}
+            {pagesArray.map(p => <span className={p === props.currentPage ? st.currentPage:''}
                                        onClick={ () => changeCurrentPage(p) }>{p} </span>)}
 
             {pagesArray[pagesArray.length-1] < totalPages - 2 ? <span
                 onClick={ () => changeCurrentPage(totalPages) }> ..{totalPages}</span>:''}
 
-            </div>
-
-            {this.props.users.map(u => <div className={st.userItem}>
-                <img className={st.profileImg}
-                     src={u.photos.small != null ? u.photos.small : 'https://cdn-icons-png.flaticon.com/512/219/219983.png'}/>
-                <div className={st.itemBlock}>
-                    <div className={st.userInfo}>
-                        <div>
-                            <div>{u.name}</div>
-                            <div className={st.location}>u.location.city, u.location.country</div>
-                        </div>
-                        <div className={st.status}>{u.status}</div>
-                    </div>
-                    <button onClick={() => {
-                        this.props.subscriptionAction(u.id, u.followed)
-                    }}
-                            className={st.button}>{(u.followed) ? 'Unfollow' : 'Follow'}</button>
-                </div>
-            </div>)}
         </div>
-    }
+
+        {props.users.map(u => <div className={st.userItem}>
+            <img className={st.profileImg}
+                 src={u.photos.small != null ? u.photos.small : 'https://cdn-icons-png.flaticon.com/512/219/219983.png'}/>
+            <div className={st.itemBlock}>
+                <div className={st.userInfo}>
+                    <div>
+                        <div>{u.name}</div>
+                        <div className={st.location}>u.location.city, u.location.country</div>
+                    </div>
+                    <div className={st.status}>{u.status}</div>
+                </div>
+                <button onClick={() => {
+                    props.subscriptionAction(u.id, u.followed)
+                }}
+                        className={st.button}>{(u.followed) ? 'Unfollow' : 'Follow'}</button>
+            </div>
+        </div>)}
+    </div>
 }
 
 export default Finder
--- src/components/Content/Finder/FinderContainer.jsx
+++ src/components/Content/Finder/FinderContainer.jsx
@@ -1,5 +1,4 @@
 import {connect} from "react-redux";
-import Finder from "./Finder";
 import {
     changePageAC,
     followAC,
@@ -7,6 +6,42 @@ import {
     totalCountAC,
     unfollowAC
 } from "../../../Redux/Reducer/FinderPageReducer";
+import React from "react";
+import * as axios from "axios";
+import Finder from "./Finder";
+
+class FinderAPI extends React.Component {
+    componentDidMount() {
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalCount(response.data.totalCount)
+            })
+    }
+
+    componentWillUnmount() {
+        this.props.changePage(1)
+    }
+
+    changeCurrentPage = (p) => {
+        this.props.changePage(p)
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${p}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalCount(response.data.totalCount)
+            })
+    }
+
+    render = () => <Finder changeCurrentPage={this.changeCurrentPage}
+                           subscriptionAction={this.props.subscriptionAction}
+                           users={this.props.users}
+                           totalCount={this.props.totalCount}
+                           currentPage={this.props.currentPage}
+                           pageSize={this.props.pageSize} />
+
+}
 
 const mapStateToProps = state => ({
     users: state.finderPage.users,
@@ -34,6 +69,6 @@ const mapDispatchToProps = dispatch => ({
     }
 })
 
-const FinderContainer = connect(mapStateToProps, mapDispatchToProps)(Finder)
+const FinderContainer = connect(mapStateToProps, mapDispatchToProps)(FinderAPI)
 
 export default FinderContainer
